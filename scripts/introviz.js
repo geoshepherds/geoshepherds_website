dfe946f07c1bab242c7b200907c39b962bfa3207
owain-stratton <owain@geoshepherds.com>
2016-05-31 12:10:07 +0200

    fixed introvis positioning on smaller screens

--- scripts/introviz.js
+++ scripts/introviz.js
@@ -50,13 +50,13 @@ $(document).ready( function() {
         height = 800,
         white = '#fff';
     
-    if($(window).width() >= 1280) {
+    if($(window).width() >= 1440) {
 
         var windowH = $(window).height() / 3,
             windowW = $(window).width() * 0.45,
             scale = 1000;
 
-    } else if($(window).width() < 1280 && $(window).width() >= 1024) {
+    } else if($(window).width() < 1440 && $(window).width() >= 1024) {
 
         var windowH = $(window).height() / 2,
             windowW = $(window).width() * 0.8,
